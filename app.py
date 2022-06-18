9bc1d43687f770c8300932533895a4acf5b582a7
vinayak <vinayaksingoriya@gmail.com>
2022-06-18 14:37:29 +0530

    App configured

--- app.py
+++ app.py
@@ -8,63 +8,96 @@ app = Flask(__name__)
 
 # Database remote connection
 db = pymysql.connect(
-            host='record-testing.cmjf6xf1uo4z.us-east-1.rds.amazonaws.com',
+            host='todoapp-db.cmjf6xf1uo4z.us-east-1.rds.amazonaws.com',
             user='admin',
+            port=3306,
             password='vinayak123',
-            database='vinayakRecord',
+            database='TodoApp',
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor)
 
 # Routing
 
+#Handling error 404 and displaying relevant web page
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'),404
+ 
+#Handling error 500 and displaying relevant web page
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'),500
+
+@app.errorhandler(502)
+def bad_gateway(error):
+    return render_template('502.html'),502
+
+
+
 @app.route("/")
 def home():
-    sql_select_Query = "select * from record"
-    cursor = db.cursor()
-    cursor.execute(sql_select_Query )
-    todo_list =  cursor.fetchall()  
-    return render_template("base.html", todo_list=todo_list)
+    try:
+        sql_select_Query = "select * from record"
+        cursor = db.cursor()
+        cursor.execute(sql_select_Query )
+        todo_list =  cursor.fetchall()  
+        return render_template("base.html", todo_list=todo_list)
+    except Exception as e : 
+        return render_template("custom_error.html", error=e)
 
 # DONE
 @app.route("/add", methods=["POST"])
 def add():
-    cursor = db.cursor()
-    title = request.form.get("title")
-    sql = """INSERT INTO record(title, complete) VALUES(%s, %s)"""
-    data = (title, False)
-    cursor.execute(sql, data)
-    db.commit()
-    return redirect(url_for("home"))
+    try:
+        cursor = db.cursor()
+        title = request.form.get("title")
+        sql = """INSERT INTO record(title, complete) VALUES(%s, %s)"""
+        data = (title, False)
+        cursor.execute(sql, data)
+        db.commit()
+        return redirect(url_for("home"))
+    except Exception as e : 
+        return render_template("custom_error.html", error=e)
+    
 
 
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
-    cursor = db.cursor()
-    select_query = "select * from record where id = %s"
-    cursor.execute(select_query, todo_id)
-    record = cursor.fetchone()
-    val = not(record["complete"])
-    update_query = """update record set complete = %s where id = %s"""
-    data = (val, todo_id)
-    cursor.execute(update_query, data)
-    db.commit()
-    return redirect(url_for("home"))
+    try:
+        cursor = db.cursor()
+        select_query = "select * from record where id = %s"
+        cursor.execute(select_query, todo_id)
+        record = cursor.fetchone()
+        val = not(record["complete"])
+        update_query = """update record set complete = %s where id = %s"""
+        data = (val, todo_id)
+        cursor.execute(update_query, data)
+        db.commit()
+        return redirect(url_for("home"))
+    except Exception as e : 
+        return render_template("custom_error.html", error=e)
 
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
-    cursor = db.cursor()
-    Delete_query = """Delete from record where id = %s"""
-    cursor.execute(Delete_query, todo_id)
-    db.commit()
-    return redirect(url_for("home"))
+    try:
+        cursor = db.cursor()
+        Delete_query = """Delete from record where id = %s"""
+        cursor.execute(Delete_query, todo_id)
+        db.commit()
+        return redirect(url_for("home"))
+    except Exception as e : 
+        return render_template("custom_error.html", error=e)
 
 @app.route("/reset")
 def reset():
-    cursor = db.cursor()
-    cursor.execute("TRUNCATE TABLE record")
-    return redirect(url_for("home"))
+    try:
+        cursor = db.cursor()
+        cursor.execute("TRUNCATE TABLE record")
+        return redirect(url_for("home"))
+    except Exception as e : 
+        return render_template("custom_error.html", error=e)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0')
  
